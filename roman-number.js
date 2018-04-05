f3c631d95b8571440fb6e95a034f8838585d1252
Nathan Lunde-Berry <mr.kequc@gmail.com>
2018-04-05 21:14:52 +0200

    Add error expectation on constructor

--- roman-number.js
+++ roman-number.js
@@ -1,9 +1,28 @@
+function RomanNumber (param) {
+}
+
+////
+// TEST
+////
+
 (function () {
+
+const testCases = [
+    { param: null, error: 'value required' }
+];
 
 function stdoutRed (message) { console.log('\x1b[31m%s\x1b[0m', message); }
 function stdoutGreen (message) { console.log('\x1b[32m%s\x1b[0m', message); }
 
-stdoutRed('TEST RED');
-stdoutGreen('TEST GREEN');
+for (const testCase of testCases) {
+    try {
+        new RomanNumber(testCase.param);
+        if (testCase.error) {
+            stdoutRed(`Expected "${testCase.param}" to throw "${testCase.error}"`);
+            continue;
+        }
+    } catch (e) {
+    }
+}
 
 })();
