8ddf6178cff81e36e3346dea15d6be4291c86cbf
Nathan Lunde-Berry <mr.kequc@gmail.com>
2018-04-05 22:07:11 +0200

    Begin expecting more complex roman numbers ensure failing test cases are shown

--- roman-number.js
+++ roman-number.js
@@ -17,13 +17,20 @@ RomanNumber.prototype.toInt = function () {
     return this._int;
 };
 
-RomanNumber.prototype.toString = function () {
-    let result = '';
-    while (this._int > 0) {
-        result += 'I';
-        this._int--;
+const DICT = [
+    { int: 4, rom: 'IV' },
+    { int: 1, rom: 'I' }
+];
+
+function toRoman (int) {
+    if (int === 0) return '';
+    for (const obj of DICT) {
+        if (int >= obj.int) return obj.rom + toRoman(int - obj.int);
     }
-    return result;
+}
+
+RomanNumber.prototype.toString = function () {
+    return toRoman(this._int);
 };
 
 ////
@@ -37,7 +44,8 @@ const testCases = [
     { param: '', error: 'value required' },
     { param: 0, error: 'invalid range' },
     { param: 1, int: 1, rom: 'I' },
-    { param: 3, int: 3, rom: 'III' }
+    { param: 3, int: 3, rom: 'III' },
+    { param: 4, int: 4, rom: 'IV' }
 ];
 
 function stdoutRed (message) { console.log('\x1b[31m%s\x1b[0m', message); }
@@ -65,11 +73,18 @@ for (const testCase of testCases) {
     const instance = buildInstance(testCase.param, testCase.error);
     if (instance === undefined) continue;
 
-    if (instance.toInt() === testCase.int) {
+    const int = instance.toInt();
+    if (int === testCase.int) {
         stdoutGreen(`Success ${testCase.param} toInt() returns ${testCase.int}`);
+    } else {
+        stdoutRed(`Expected ${testCase.param} toInt() to return ${testCase.int} instead got ${int}`);
     }
-    if (instance.toString() === testCase.rom) {
+
+    const rom = instance.toString();
+    if (rom === testCase.rom) {
         stdoutGreen(`Success ${testCase.param} toString() returns ${testCase.rom}`);
+    } else {
+        stdoutRed(`Expected ${testCase.param} toString() to return ${testCase.rom} instead got ${rom}`);
     }
 }
 
